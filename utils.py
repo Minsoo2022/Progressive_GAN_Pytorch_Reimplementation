8881c2f2a45523c725c384080c0e93c229e1085f
Minsoo2022 <54705353+Minsoo2022@users.noreply.github.com>
2020-12-26 19:08:48 +0900

    add clamp
    add clamp

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 def add_images(board, images, stage, iteration, name) :
     #cliping
-    board.add_images(str(stage)+'/'+name, (images / 2 + 0.5), iteration)
+    board.add_images(str(stage)+'/'+name, (images / 2 + 0.5).clamp(0,1), iteration)
 
 def add_losses(board, losses, stage, iteration) :
     keys = losses.keys()
